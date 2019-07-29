312c00c39cd221565e861e2dfe5a591fa90cff75
luwenshan <lws1076694749@126.com>
2019-07-29 20:53:17 +0800

    Trie Tree

---
+++ app/src/main/java/com/lws/algorithm/standard/Trie.java
@@ -0,0 +1,50 @@
+package com.lws.algorithm.standard;
+
+/**
+ * Trie 树
+ * https://time.geekbang.org/column/article/72414
+ */
+public class Trie {
+    private TrieNode root = new TrieNode('/'); // 存储无意义字符
+
+    // 往 Trie 树中插入一个字符串
+    public void insert(char[] text) {
+        TrieNode p = root;
+        for (int i = 0; i < text.length; i++) {
+            int index = text[i] - 'a';
+            if (p.children[index] == null) {
+                TrieNode newNode = new TrieNode(text[i]);
+                p.children[index] = newNode;
+            }
+            p = p.children[index];
+        }
+        p.isEndingChar = true;
+    }
+
+    // 在 Trie 树中查找一个字符串
+    public boolean find(char[] pattern) {
+        TrieNode p = root;
+        for (int i = 0; i < pattern.length; i++) {
+            int index = pattern[i] - 'a';
+            if (p.children[index] == null) {
+                return false;
+            }
+            p = p.children[index];
+        }
+        if (p.isEndingChar == false) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public static class TrieNode {
+        public char data;
+        public TrieNode[] children = new TrieNode[26];
+        public boolean isEndingChar = false;
+
+        public TrieNode(char data) {
+            this.data = data;
+        }
+    }
+}
